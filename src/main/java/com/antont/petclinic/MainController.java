387dcbaa9d21e8e9e627110e5ead4728ef479d07
Turchenko Anton <turchenkoav96@gmail.com>
2021-06-03 18:11:07 +0300

    task#10 Create a page with information about the owner's pets https://app.tmetric.com/#/tasks/118627/2884615 Created drafts of user and admin page, added main controller with mapping for new pages Modified home page(index.html)

---
+++ src/main/java/com/antont/petclinic/MainController.java
@@ -0,0 +1,23 @@
+package com.antont.petclinic;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+    @GetMapping(value = {"/", "/index"})
+    public String index() {
+        return "/index";
+    }
+
+    @GetMapping("/admin")
+    public String admin() {
+        return "/admin";
+    }
+
+    @GetMapping("/user")
+    public String user() {
+        return "/user";
+    }
+}
