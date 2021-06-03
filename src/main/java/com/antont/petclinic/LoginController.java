0fdd3369aaabe1217e39ba21ce6bbb57436f80a2
Turchenko Anton <turchenkoav96@gmail.com>
2021-06-03 18:04:49 +0300

    task#7 Create sign in page https://app.tmetric.com/#/tasks/118627/2879348 Reworked draft of login page and LoginController

--- src/main/java/com/antont/petclinic/LoginController.java
+++ src/main/java/com/antont/petclinic/LoginController.java
@@ -1,11 +1,13 @@
 package com.antont.petclinic;
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
 public class LoginController {
 
-    @RequestMapping("/login.html")
+    @GetMapping("/login")
     public String login() {
-        return "login";
+        return "/login";
     }
 }
