f98bb9a337894cedbb939526be6477036ac8ccdf
Turchenko Anton <turchenkoav96@gmail.com>
2021-06-03 17:56:20 +0300

    task#19 Implement sign in logic https://app.tmetric.com/#/tasks/118627/2880666 Modified Security Configuration added different user roles Created CustomAccessDeniedHandler

--- src/main/java/com/antont/petclinic/security/SecurityConfig.java
+++ src/main/java/com/antont/petclinic/security/SecurityConfig.java
@@ -1,28 +1,57 @@
 package com.antont.petclinic.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.access.AccessDeniedHandler;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
-    protected void configure(final AuthenticationManagerBuilder auth) throws Exception {
-        // authentication manager (see below)
-    }
+    protected void configure(HttpSecurity http) throws Exception {
 
-    @Override
-    protected void configure(final HttpSecurity http) throws Exception {
-        http
+        http.csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/", "/index", "/about").permitAll()
+                .antMatchers("/admin/**").hasAnyRole("ADMIN")
+                .antMatchers("/user/**").hasAnyRole("USER")
+                .anyRequest().authenticated()
+                .and()
                 .formLogin()
-                .loginPage("/login.html")
-                .failureUrl("/error.html")
+                .loginPage("/login")
+                .permitAll()
                 .and()
                 .logout()
-                .logoutSuccessUrl("/index.html");
+                .logoutSuccessUrl("/")
+                .permitAll()
+                .and()
+                .exceptionHandling().accessDeniedHandler(accessDeniedHandler());
     }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+
+        auth.inMemoryAuthentication()
+                .withUser("user").password(passwordEncoder().encode("password")).roles("USER")
+                .and()
+                .withUser("admin").password(passwordEncoder().encode("admin")).roles("ADMIN");
+    }
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AccessDeniedHandler accessDeniedHandler() {
+        return new CustomAccessDeniedHandler();
+    }
+
 }
