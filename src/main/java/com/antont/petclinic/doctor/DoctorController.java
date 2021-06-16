ea31dd010f2f1682897b493e903f9cfbf311116e
Turchenko Anton <turchenkoav96@gmail.com>
2021-06-16 18:04:25 +0300

    task#27 implement adding a new issue https://app.tmetric.com/#/tasks/118627/2918101 Added functionality to add a new issue

--- src/main/java/com/antont/petclinic/doctor/DoctorController.java
+++ src/main/java/com/antont/petclinic/doctor/DoctorController.java
@@ -1,21 +1,23 @@
 package com.antont.petclinic.doctor;
 
 import com.antont.petclinic.user.issues.Issue;
+import com.antont.petclinic.user.issues.IssueDto;
 import com.antont.petclinic.user.issues.IssuesService;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 @Controller
+@RequestMapping("/doctor/issues")
 public class DoctorController {
 
     final private IssuesService issuesService;
@@ -24,38 +26,11 @@ public class DoctorController {
         this.issuesService = issuesService;
     }
 
-    @GetMapping("/doctor/issues")
-    public String findPaginated(@RequestParam("page") Optional<Integer> page,
-                                @RequestParam("size") Optional<Integer> size,
-                                @RequestParam("sortField") Optional<String> sortField,
-                                @RequestParam("sortDir") Optional<String> sortDir,
-                                Model model) {
-        int currentPage = page.orElse(1);
-        int pageSize = size.orElse(5);
-        String currentSortField = sortField.orElse("id");
-        String currentSortDir = sortDir.orElse("asc");
+    @PostMapping("/new")
+    @PreAuthorize("hasAnyRole('ROLE_DOCTOR','ROLE_ADMIN')")
+    public String addNewIssue(@Valid @ModelAttribute IssueDto issueDto) {
+        issuesService.addNewIssue(issueDto);
 
-        Sort sort = currentSortDir.equalsIgnoreCase(Sort.Direction.ASC.name()) ? Sort.by(currentSortField).ascending() :
-                Sort.by(currentSortField).descending();
-
-        Page<Issue> issuesPage = issuesService.findPaginatedForDoctor(PageRequest.of(currentPage - 1, pageSize, sort));
-
-        model.addAttribute("issuesPage", issuesPage);
-
-        model.addAttribute("currentPage", currentPage);
-        model.addAttribute("size", pageSize);
-        model.addAttribute("sortField", currentSortField);
-        model.addAttribute("sortDir", currentSortDir);
-        model.addAttribute("reverseSortDir", currentSortDir.equals("asc") ? "desc" : "asc");
-
-        int totalPages = issuesPage.getTotalPages();
-        if (totalPages > 0) {
-            List<Integer> pageNumbers = IntStream.rangeClosed(1, totalPages)
-                    .boxed()
-                    .collect(Collectors.toList());
-            model.addAttribute("pageNumbers", pageNumbers);
-        }
-
-        return "/issues";
+        return "redirect:/issues";
     }
 }
