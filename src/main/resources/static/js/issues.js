e726af4f99c00d5202aa02cdce27dfff4c45fe6f
Turchenko Anton <turchenkoav96@gmail.com>
2021-06-23 12:45:06 +0300

    task#27 implement adding a new issue https://app.tmetric.com/#/tasks/118627/2918101 Fixed edit and delete issue functionality

--- src/main/resources/static/js/issues.js
+++ src/main/resources/static/js/issues.js
@@ -1,13 +1,13 @@
-let petsList = {};
+const CONTEXT_PATH = $('#contextPathHolder').attr('data-contextPath');
 
-function editPet(index, petElements) {
-    petsList = petElements;
+
+function editPet(index) {
     let elements = $(".tablerow-" + index).children()
     let selectedPet = elements[1].innerHTML;
     elements.eq(1).html("");
     const sel = $("<select id='editPet-" + index + "' class='form-control'>").appendTo(elements.eq(1));
     sel.append($("<option>").attr('value', "").text("Select pet"));
-    $(petElements).each(function () {
+    $(petsData).each(function () {
         sel.append($("<option>").attr('value', this.id).text(this.name + ", owner: " + this.owner));
     });
 
@@ -34,7 +34,7 @@ function sendUpdatePetRequest(index) {
         type: "PUT",
         contentType: "application/json",
         dataType: 'json',
-        url: "api/issues/?id=" + issueId,
+        url: CONTEXT_PATH + "api/issues?id=" + issueId,
         data: JSON.stringify(issueRequestModel),
         success: function (response) {
             if (response.status === "ok") {
@@ -58,26 +58,25 @@ function sendUpdatePetRequest(index) {
 function updatePetInfo(updatedPetInfo, index) {
     let elements = $(".tablerow-" + index).children();
 
+    let issueId = elements[0].innerHTML;
+
     elements.eq(1).html(updatedPetInfo.petName);
     elements.eq(2).html(updatedPetInfo.doctorFullName);
     elements.eq(3).html(updatedPetInfo.description);
     elements.eq(4).html(updatedPetInfo.date);
-    elements.eq(5).html(`<button type="button" class="btn btn-primary"
-                                onclick="editPet(${index}, ${petsList})">Edit</button>`);
+    elements.eq(5).html(`<button type="button" class="btn btn-primary" onclick="editPet(${index})">Edit</button>
+                          <button type="button" class="btn btn-danger" onclick="deletePet(${issueId}, ${index})">Delete</button>`);
 }
 
-function deletePet(id) {
+function deletePet(id, index) {
     const result = confirm("Are you sure you want to delete issue record?");
     if (result) {
         $.ajax({
             type: "DELETE",
             contentType: "application/json",
-            dataType: 'json',
-            url: "api/issues/?id=" + id,
-            success: function (response) {
-                if (response.status === "ok") {
-
-                }
+            url: CONTEXT_PATH + "api/issues?id=" + id,
+            success: function () {
+                location.reload()
             },
             error: function (e) {
                 console.log("error: " + JSON.stringify(e));
